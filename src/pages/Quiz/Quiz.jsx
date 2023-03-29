de24490b4c66688a7de12a1cbde026c1c2bcd523
stivex001 <stephenadeyemo@gmail.com>
2023-03-29 20:24:09 +0100

    feat: added input field

--- src/pages/Quiz/Quiz.jsx
+++ src/pages/Quiz/Quiz.jsx
@@ -1,33 +1,44 @@
+import { MenuItem, TextField } from "@mui/material";
 import styled from "styled-components";
-import hero from '../../assets/hero.png'
+import hero from "../../assets/hero.png";
+import Categories from "../../Data/categories";
 import { mobile } from "../../responsive";
 
 const Container = styled.div`
-display: flex;
-justify-content: space-around;
-/* align-items: center; */
-${mobile({ flexDirection: "column-reverse", alignItems: "center"})}
+  display: flex;
+  justify-content: space-around;
+  /* align-items: center; */
+  ${mobile({ flexDirection: "column-reverse", alignItems: "center" })}
 `;
 const Settings = styled.div`
-padding: 10px;
-display: flex;
-flex-direction: column;
-flex: 1;
-align-items: center;
-font-weight: 300;
-${mobile({ width: "100%"})}
+  padding: 10px;
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  align-items: center;
+  font-weight: 300;
+  ${mobile({ width: "100%" })}
 `;
 const Span = styled.span`
-font-size: 30px;
+  font-size: 30px;
 `;
 const ImgContainer = styled.div`
-flex: 2;
+  flex: 1.5;
 `;
 const Img = styled.img`
-width: 100%;
-align-self: center;
-padding: 8px;
-${mobile({ width: "85%", padding: "20px 0 0 0 "})}
+  width: 70%;
+  align-self: center;
+  padding: 8px;
+  ${mobile({ width: "85%", padding: "20px 0 0 0 " })}
+`;
+
+const Forms = styled.form`
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+  width: 100%;
+  flex: 0.8;
+  gap: 50px;
 `;
 
 const Quiz = () => {
@@ -35,6 +46,29 @@ const Quiz = () => {
     <Container>
       <Settings>
         <Span>Quiz Settings</Span>
+        <Forms>
+          <TextField label="Enter Your Name" variant="outlined" />
+
+          <TextField select label="Select Category" variant="outlined">
+            {Categories.map((cat) => (
+              <MenuItem key={cat.category} value={cat.value}>
+                {cat.category}
+              </MenuItem>
+            ))}
+          </TextField>
+
+          <TextField select label="Select Difficulty" variant="outlined">
+            <MenuItem key="Easy" value="easy">
+              Easy
+            </MenuItem>
+            <MenuItem key="Medium" value="medium">
+              Medium
+            </MenuItem>
+            <MenuItem key="Medium" value="medium">
+              Hard
+            </MenuItem>
+          </TextField>
+        </Forms>
       </Settings>
       <ImgContainer>
         <Img src={hero} alt="quiz-image" />
