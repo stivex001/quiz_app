622fee1de2a3d73d8885dd4ede075cac85952867
stivex001 <stephenadeyemo@gmail.com>
2023-03-29 19:35:49 +0100

    fix: typo fixed

--- src/pages/Quiz/Quiz.jsx
+++ src/pages/Quiz/Quiz.jsx
@@ -12,6 +12,7 @@ const Settings = styled.div`
 padding: 10px;
 display: flex;
 flex-direction: column;
+flex: 1;
 align-items: center;
 font-weight: 300;
 ${mobile({ width: "100%"})}
@@ -19,9 +20,11 @@ ${mobile({ width: "100%"})}
 const Span = styled.span`
 font-size: 30px;
 `;
-const ImgContainer = styled.div``;
+const ImgContainer = styled.div`
+flex: 2;
+`;
 const Img = styled.img`
-width: 55%;
+width: 100%;
 align-self: center;
 padding: 8px;
 ${mobile({ width: "85%", padding: "20px 0 0 0 "})}
