c33a794d0b8ace3df7fc96e77e60bdc68f70588e
stivex001 <stephenadeyemo@gmail.com>
2023-04-02 19:24:58 +0100

    added stylings to the quizdata page

--- src/Data/questions.js
+++ src/Data/questions.js
@@ -1,17 +1,17 @@
-const quizData = [
+export const quizData = [
   {
     id: 1,
     question: "What is the capital of France?",
     options: ["London", "Paris", "New York", "Tokyo"],
     answer: "Paris",
-    category: 'General Knowledge'
+    category: "General Knowledge",
   },
   {
     id: 2,
     question: "What is the currency of Japan?",
     options: ["Dollar", "Pound", "Yen", "Euro"],
     answer: "Yen",
-    category: 'General Knowledge'
+    category: "General Knowledge",
   },
   {
     id: 3,
@@ -22,94 +22,8 @@ const quizData = [
       "Guglielmo Marconi",
       "Nikola Tesla",
     ],
+
     answer: "Alexander Graham Bell",
-    category: 'General Knowledge'
+    category: "General Knowledge",
   },
 ];
-
-export default quizData;
-
-const QuizForm = () => {
-  const [selectedAnswer, setSelectedAnswer] = useState("");
-
-  const handleOptionClick = (answer) => {
-    setSelectedAnswer(answer);
-  };
-
-  return (
-    <QuizContainer>
-      {quizData.map((quizItem, index) => (
-        <div key={index}>
-          <Question>{quizItem.question}</Question>
-          {quizItem.options.map((option, optionIndex) => (
-            <Option
-              key={optionIndex}
-              isSelected={selectedAnswer === option}
-              onClick={() => handleOptionClick(option)}
-            >
-              {option}
-            </Option>
-          ))}
-        </div>
-      ))}
-    </QuizContainer>
-  );
-};
-
-const Option = styled.div`
-  background-color: ${(props) => (props.isSelected ? "#4CAF50" : "#ffffff")};
-  color: ${(props) => (props.isSelected ? "#ffffff" : "#000000")};
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  padding: 10px;
-  margin-bottom: 10px;
-  cursor: pointer;
-`;
-
-return (
-  <QuizContainer>
-    {quizData.map((quizItem, index) => (
-      <div key={index}>
-        <Question>{quizItem.question}</Question>
-        {quizItem.options.map((option, optionIndex) => (
-          <Option
-            key={optionIndex}
-            isSelected={selectedAnswer === option}
-            onClick={() => handleOptionClick(option)}
-          >
-            {option}
-          </Option>
-        ))}
-      </div>
-    ))}
-  </QuizContainer>
-);
-
-// const fetchQuestions = [
-//     {
-//         id: 1,
-//         category: 'Entertainment: Television',
-//         correct_answer: 'Radar',
-//         difficulty: 'easy',
-//         question: 'What is a company ',
-//         type: 'multiple',
-//         incorrect_answers: [
-//             {
-//                 id: 1,
-//                 answer: 'Hawkeye'
-
-//             },
-//             {
-//                 id: 2,
-//                 answer: 'Hot Lips',
-
-//             },
-//             {
-//                 id: 3,
-//                 answer: 'Trapper',
-
-//             }
-//         ]
-
-//     }
-// ]
--- src/pages/QuizTable/QuizData.jsx
+++ src/pages/QuizTable/QuizData.jsx
@@ -1,9 +1,65 @@
-import React from 'react'
+import React, { useState } from "react";
+import styled from "styled-components";
+import { quizData } from "../../Data/questions";
+
+const Table = styled.table`
+  border-collapse: collapse;
+  width: 100%;
+  max-width: 800px;
+  margin: 0 auto;
+  th,
+  td {
+    padding: 0.5rem;
+    text-align: left;
+    border: 1px solid #ccc;
+  }
+  th {
+    background-color: #f2f2f2;
+  }
+  td button {
+    margin-right: 0.5rem;
+  }
+`;
 
 const QuizData = () => {
-  return (
-    <div>QuizData</div>
-  )
-}
+  const [data, setData] = useState(quizData);
 
-export default QuizData
+  const handleDelete = (id) => {
+    setData(data.filter((item) => item.id !== id));
+  };
+
+  const handleEdit = (id) => {
+    // Implement edit functionality here
+    console.log(`Edit ${id}`);
+  };
+
+  return (
+    <Table>
+      <thead>
+        <tr>
+          <th>Question</th>
+          <th>Options</th>
+          <th>Answer</th>
+          <th>Category</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((item) => (
+          <tr key={item.id}>
+            <td>{item.question}</td>
+            <td>{item.options.join(", ")}</td>
+            <td>{item.answer}</td>
+            <td>{item.category}</td>
+            <td>
+              <button onClick={() => handleEdit(item.id)}>Edit</button>
+              <button onClick={() => handleDelete(item.id)}>Delete</button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
+  );
+};
+
+export default QuizData;
